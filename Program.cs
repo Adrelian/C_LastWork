fe290a2696eb77430a21e4bcf229096778ca880e
Anatolii <anatolii.polovnikov@gmail.com>
2022-12-26 17:08:30 +0500

    закончил решение

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 ﻿void userArray ()
 {
     // Пользовательский ввод размера массива
+    
     Console.Write("Введите размер массива данных ");
     int size = Convert.ToInt32(Console.ReadLine());
 
     string [] UserFirstArray = new string [size]; // Массив пользовательских строк
-    string [] UserSecondArray = new string [size]; // Массив строк, где длянна символом меньше 3
+    string [] UserSecondArray = new string [size]; // Массив строк, где длина символом меньше 3
 
     // Вводим каждый новый элемент массива построчно
 
     for (int i = 0; i < size; i++)
     {
-        string element;
-        Console.Write($"Введите {i+1} массива ");
+        string element = string.Empty;
+        Console.Write($"Введите {i+1} элемент массива ");
         element = Console.ReadLine();
         if (element.Length < 3)
         {
@@ -22,19 +23,22 @@
     }
     // Вывод массива пользователя
     Console.Write("Массив пользователя -> ");
-        for (int i = 0; i < size; i++)
-        {
-            Console.Write(UserFirstArray[i] + ", ");
-        }
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(UserFirstArray[i] + ", ");
+    }
 
     Console.WriteLine(); // отступ
 
-    // Вывод массива пользователя
+    // Вывод массива пользователя  с элементами длиной меньше 3
     Console.Write("Массив элементов длинной меньше 3 символов -> ");
-        for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
+    {
+        if (UserSecondArray[i] != null)
         {
-            Console.Write(UserSecondArray[i] + ", ");
-        } 
+            Console.Write(UserSecondArray[i] + ", ");  
+        }
+    } 
 }
 
 userArray();
